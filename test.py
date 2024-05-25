857815ade669949033d0ecd07acae22b13c5a9b4
Your Name <you@example.com>
2024-05-25 02:06:20 +0100

    Feature signup working

---
+++ test.py
@@ -0,0 +1,41 @@
+#This is a comment.
+print("Welome World")
+print("Hello Sir") # Hello Sir
+
+# print "YES" if 5 is larger than 2.
+print("Yes")
+
+
+if 5 > 2:
+  print("Five is greater than two!")
+
+#variable
+  x = 5
+y = "John"
+print(x)
+print(y)
+
+x = 4       # x is of type int
+x = "Sally" # x is now of type str
+print(x)
+
+#casting
+x = str(3)    # x will be '3'
+y = int(3)    # y will be 3
+z = float(3)  # z will be 3.0
+
+#variable with the type() function
+x = 5
+y = "John"
+print(type(x))
+print(type(y))
+
+#double quotes
+x = "John"
+# is the same as
+x = 'John'
+
+#two variables
+a = 4
+A = "Sally"
+#A will not overwrite a
